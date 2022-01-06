4982daaacc2b8b3ec47aa21dd2993a6736779bc0
Kalvin Wettstein <kalvinwettstein@gmail.com>
2022-01-06 15:37:33 -0700

    commit files

---
+++ MyFirstASPNETWebApp/wwwroot/js/fanMail.js
@@ -0,0 +1,17 @@
+﻿// Normal JS
+//document.getElementById('btnsend').addEventListener("click", function () {
+//    alert('From: ' + document.getElementById("textFrom").value +
+//        ", Subject: " + document.getElementById("textSubject").value +
+//        ", Message: " + document.getElementById("textMessage").value);
+//});
+
+
+$('#btnsend').click(function () {
+    alert("From: " + $("#textFrom").val() +
+        ", Subject: " + $("#textSubject").val() +
+        ", Message: " + $("#textMessage").val());
+
+    $("#picGanderson").fadeToggle();
+
+});
+
