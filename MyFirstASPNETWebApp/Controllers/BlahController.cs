529b3993439c258bf11ec1885af313a0989a0fcb
Kalvin Wettstein <kalvinwettstein@gmail.com>
2022-01-17 21:53:43 -0700

    mission three almost done

--- MyFirstASPNETWebApp/Controllers/BlahController.cs
+++ MyFirstASPNETWebApp/Controllers/BlahController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyFirstASPNETWebApp.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,7 +17,14 @@ namespace MyFirstASPNETWebApp.Controllers
             return View();
         }
 
-        public IActionResult FanMail()
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculatorModel model)
         {
             return View();
         }
---
+++ MyFirstASPNETWebApp/Models/GradeCalculatorModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNETWebApp.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Required(ErrorMessage = "test")]
+        [Range(0, 100,
+            ErrorMessage = "Assign must be between 0 and 100")]
+        public int? Assign { get; set; }
+
+        [Required(ErrorMessage = "Please enter a value")]
+        [Range(0, 100,
+            ErrorMessage = "Group Project must be between 0 and 100")]
+        public int? Gp { get; set; }
+
+        [Required(ErrorMessage = "Please enter a value")]
+        [Range(0, 100,
+            ErrorMessage = "Quiz must be between 0 and 100")]
+        public int? Qz { get; set; }
+
+        [Required(ErrorMessage = "Please enter a value")]
+        [Range(0, 100,
+            ErrorMessage = "Exam must be between 0 and 100")]
+        public int? Ex { get; set; }
+
+        [Required(ErrorMessage = "Please enter a value")]
+        [Range(0, 100,
+            ErrorMessage = "Intex must be between 0 and 100")]
+        public int? Intex { get; set; }
+    }
+}
+
+// Custom getter and setter
+//Setter
+//public void setAssign(int input)
+//{
+//    this.Assign = input;
+//}
+
+//Getter
+//public int getAssign()
+//{
+//    return this.Assign;
+//}
